8b90c344708ecd6368b5ed134c3ecbd12210eb91
Yaroslav Virstiuk <yarovji@gmail.com>
2019-09-04 08:30:51 +0300

    2 module done

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -10,7 +10,14 @@ export class CarsComponent implements OnInit {
   canAddCarStatus = false;
   // inputText = '';
   carName = '';
-  cars = ['Ford', 'Audi', 'BMW'];
+  cars = ['Ford', 'Audi', 'BMW', 'Renault', 'Skoda'];
+  items = [
+    new Date(2015, 3, 23),
+    new Date(2017, 5, 30),
+    new Date(2006, 1, 4),
+    new Date(2016, 4, 4),
+    new Date(2000, 11, 10)
+  ];
 
   constructor() { }
 
