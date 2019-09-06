01475a80346c715b5a22d7db2d6027604e504f83
Yaroslav Virstiuk <yarovji@gmail.com>
2019-09-06 11:18:14 +0300

    angular pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bigbigbig';
-  text = 'Створення кастомної дерективи';
-  items = [1, 2, 3, 4, 5];
-  current = 1;
+  // text = 'Створення кастомної дерективи';
+  // items = [1, 2, 3, 4, 5];
+  // current = 1;
 
-  onClick(num: number) {
-    this.current = num;
-  }
+  // onClick(num: number) {
+  //   this.current = num;
+  // }
+
+  name = 'WebForMyFuture';
+  pi = Math.PI;
+  money = 350;
+  date = new Date();
+  amount = 0.45;
+  object = {
+    fn: 'Yaro',
+    sn: 'Vji',
+    about: {
+      old: 29,
+      work: [2009, 2019]
+    }
+  };
+
 }
