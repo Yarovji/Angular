4d3aa2d8ae48433954473bd123b263a3b8b2fb84
Yaroslav Virstiuk <yarovji@gmail.com>
2019-09-07 12:10:22 +0300

    timpl-dr-forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+@ViewChild('myForm') myForm: NgForm;
+
   answers = [{
     type: 'yes',
     text: 'так'
@@ -14,4 +17,31 @@ export class AppComponent {
     type: 'no',
     text: 'ні'
   }];
+
+  defoultcountry = 'ua';
+  defoultanswer = 'yes';
+
+  formData = {};
+  isSubmited = false;
+
+  submitForm() {
+    console.log(this.myForm.value);
+    this.formData = this.myForm.value;
+    this.isSubmited = true;
+    this.myForm.reset();
+  }
+
+  addRandomMail() {
+    // this.myForm.setValue({
+    //   email: 'ggg@ggg',
+    //   pass: '123',
+    //   country: 'pl',
+    //   question: 'no'
+    // });
+    this.myForm.form.patchValue({
+      email: 'sss@sss'
+    });
+
+
+  }
 }
