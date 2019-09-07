1b98cb2db191c9ce0fc1cc631e3fb3025b2d6fa6
Yaroslav Virstiuk <yarovji@gmail.com>
2019-09-07 17:34:43 +0300

    http module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { reject } from 'q';
+import { Component,  } from '@angular/core';
 
 
 @Component({
@@ -8,44 +6,14 @@ import { reject } from 'q';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
+  cars = [
+    {
+      name: 'Ford',
+      color: 'white',
+      id: 1
+    }
+  ];
 
-  answers = [{
-    type: 'yes',
-    text: 'так'
-  },
-  {
-    type: 'no',
-    text: 'ні'
-  }];
-
-  myform: FormGroup;
-
-  ngOnInit() {
-    this.myform = new FormGroup({
-      email: new FormControl('', [Validators.required, Validators.email], this.checkForEmail),
-      pass: new FormControl('', Validators.required),
-      country: new FormControl('ua'),
-      answer: new FormControl('')
-    });
-  }
-
-  onSubmit() {
-    console.log(this.myform)
-  }
-
-  checkForEmail(control: FormControl): Promise<any> {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if (control.value === 'test@i.ua') {
-          resolve({
-            emailIsUsed: true
-          });
-        } else {
-          resolve(null);
-        }
-      }, 3000);
-    });
-  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -14,7 +15,8 @@ import { ReactiveFormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
