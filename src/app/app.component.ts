96841d31588d2465e644fa151a4b2397ce0c0fcd
Yaroslav Virstiuk <yarovji@gmail.com>
2019-09-07 09:56:21 +0300

    forms start m#7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,29 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bigbigbig';
-  // text = 'Створення кастомної дерективи';
-  // items = [1, 2, 3, 4, 5];
-  // current = 1;
-
-  // onClick(num: number) {
-  //   this.current = num;
-  // }
-
-  // name = 'WebForMyFuture';
-  // pi = Math.PI;
-  // money = 350;
-  // date = new Date();
-  // amount = 0.45;
-  // object = {
-  //   fn: 'Yaro',
-  //   sn: 'Vji',
-  //   about: {
-  //     old: 29,
-  //     work: [2009, 2019]
-  //   }
-  // };
-
-  num = 4;
-
+  answers = [{
+    type: 'yes',
+    text: 'так'
+  },
+  {
+    type: 'no',
+    text: 'ні'
+  }];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CarComponent } from './car/car.component';
-import { CarsComponent } from './cars/cars.component';
 import { FormsModule } from '@angular/forms';
-import { AddCarComponent } from './add-car/add-car.component';
-import { BgDirective } from './directives/bg.directive';
-import { PowPipe } from './pipes/pow.pipe';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarComponent,
-    CarsComponent,
-    AddCarComponent,
-    BgDirective,
-    PowPipe
+
   ],
   imports: [
     BrowserModule,
