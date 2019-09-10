3e3e0eeb7ca293ce1aab62a51c8e9a7409bec0c9
Yaroslav Virstiuk <yarovji@gmail.com>
2019-09-10 10:04:24 +0300

    m#10 routing done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { CarsService } from './cars.service';
 import { HomePageComponent } from './home-page/home-page.component';
-import { CarsPageComponent } from './cars-page/cars-page.component';
-import { CarPageComponent } from './car-page/car-page.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    CarsPageComponent,
-    CarPageComponent,
-
   ],
   imports: [
     BrowserModule,
@@ -25,7 +19,7 @@ import { CarPageComponent } from './car-page/car-page.component';
     FormsModule,
     HttpClientModule
   ],
-  providers: [CarsService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
