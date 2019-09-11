2fdf1a3ad450f542f63560ce208c23b93422fd79
Yaroslav Virstiuk <yarovji@gmail.com>
2019-09-11 12:30:32 +0300

    m#11 done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,47 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import { divrigger } from './app.animations';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  animations: [
-    trigger('clickedDiv', [
-      state('start', style({
-        backgroundColor: 'blue',
-        width: '150px',
-        height: '150px'
-      })),
-      state('end', style({
-        backgroundColor: 'red',
-        width: '300px',
-        height: '300px'
-      })),
-      state('active', style({
-        width: '170px',
-        height: '170px',
-        backgroundColor: 'yellow',
-      })),
-      // transition('start => end', animate(800)),
-      transition('end <=> start', animate('800ms 0.5s ease-out')),
-      transition('start => active', animate(400)),
-      transition('active => end', animate(400)),
-    ])
-  ]
+  animations: [divrigger]
 })
 export class AppComponent implements OnInit {
-  clickedDivState = 'start';
+  isVisible = false;
 
   constructor() {}
 
   ngOnInit(): void {}
 
-  changeDivState() {
-    this.clickedDivState = 'end';
-    setTimeout(() => {
-      this.clickedDivState = 'start';
-    }, 3000);
-  }
+
 
 
 
